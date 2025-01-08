b2b1b815460eaa452ec67a1c965adc054ade64da
luxluxlux <ap-inc@mail.ru>
2025-01-08 22:24:48 +0500

    Create windows, close #30, #40

---
+++ client/src/windows/LicenseAgreement.tsx
@@ -0,0 +1,27 @@
+// TODO Prepare text
+const LicenseAgreement = () => (
+    <div>
+        <h2>License Agreement</h2>
+        <p>
+            This Application Agreement (&quot;Agreement&quot;) is made between [Applicant&apos;s
+            Name] (&quot;Applicant&quot;) and [Organization&apos;s Name] (&quot;Organization&quot;)
+            as of [Date]. The Applicant hereby applies for [specific program, service, or
+            opportunity] offered by the Organization. By submitting this application, the Applicant
+            agrees to provide accurate and complete information as required by the Organization and
+            understands that any false statements may result in disqualification from the
+            application process.
+        </p>
+        <p>
+            Upon acceptance into the [program/service/opportunity], the Applicant agrees to abide by
+            all rules, regulations, and policies set forth by the Organization. The Applicant
+            acknowledges that participation may require adherence to specific deadlines and
+            commitments and understands that failure to comply may lead to termination of their
+            participation. This Agreement represents the entire understanding between the parties
+            regarding the application and participation in the [program/service/opportunity].
+        </p>
+    </div>
+);
+
+LicenseAgreement.displayName = 'LicenseAgreement';
+
+export default LicenseAgreement;
