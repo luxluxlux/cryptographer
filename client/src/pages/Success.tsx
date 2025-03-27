30065c972dda5ef6e0e48d679843210d1652fac4
luxluxlux <ap-inc@mail.ru>
2025-03-27 17:22:45 +0500

    Prevent multiple downloads, close #132

--- client/src/pages/Success.tsx
+++ client/src/pages/Success.tsx
@@ -1,9 +1,8 @@
-import { memo, useCallback, useEffect, useRef } from 'react';
-import { Link as RouterLink, useLocation, Navigate } from 'react-router-dom';
+import { memo, useCallback, useEffect } from 'react';
+import { Link as RouterLink, useLocation, Navigate, useNavigate } from 'react-router-dom';
 import MuiLink from '@mui/material/Link';
 import Button from '@mui/material/Button';
-import { download, ellipse, wait } from 'utils/common';
-import { BREAKPOINT, useBreakpoint } from 'utils/breakpoints';
+import { download, ellipse } from 'utils/common';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
@@ -12,41 +11,43 @@ const SHARED_TEXT =
     'I protect my files with a password using Cryptographer. Protect your files too!';
 
 const Success = () => {
-    const isDesktop = useBreakpoint(BREAKPOINT.S);
     const location = useLocation();
+    const navigate = useNavigate();
     const hostname = window.location.hostname;
-    const rootRef = useRef(null);
 
-    const handleClick = useCallback(() => {
+    const downloadFile = useCallback(() => {
         download(location.state.data, location.state.fileName);
+        navigate('/success', {
+            state: {
+                ...location.state,
+                downloaded: true,
+            },
+        });
     }, [location.state]);
 
     useEffect(() => {
-        if (location.state) {
-            (async () => {
-                // TODD Add check that the current page doesn't change
-                // Give the user some time to read the result
-                await wait(isDesktop ? 1000 : 2000);
-                // Download file only if user is still on this page
-                if (rootRef.current) {
-                    download(location.state.data, location.state.fileName);
-                }
-            })();
+        if (!location.state || location.state.downloaded) {
+            return;
         }
-    }, []);
+        // Give the user some time to read the result
+        const timeout = setTimeout(() => {
+            downloadFile();
+        }, 2000);
+        return () => clearTimeout(timeout);
+    }, [location.state]);
 
     if (!location.state) {
         return <Navigate to="/" replace />;
     }
 
     return (
-        <div ref={rootRef} className="success">
+        <div className="success">
             <p className="success__description">
                 <b title={location.state.fileName}>{ellipse(location.state.fileName, 40)}</b> was
                 successfully {location.state.action}ed and downloaded. If not, use the button below.
             </p>
             <div className="success__actions">
-                <Button variant="contained" onClick={handleClick}>
+                <Button variant="contained" onClick={downloadFile}>
                     Download
                 </Button>
                 <Button component={RouterLink} to="/" variant="outlined">
